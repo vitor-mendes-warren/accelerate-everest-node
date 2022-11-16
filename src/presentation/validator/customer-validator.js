a5586aea8a46534e264eb08a9d17325ebd112c68
vitor-mendes-warren <vitor.mendes@warren.com.br>
2022-11-16 14:01:41 -0300

    change: reuse code in number format function

--- src/presentation/validator/customer-validator.js
+++ src/presentation/validator/customer-validator.js
@@ -6,22 +6,26 @@ var cpfMaxMin = ({ 'max': 14, 'min': 11 });
 var phoneMaxMin = ({ 'max': 14, 'min': 11 });
 var postalCodeMaxMin = ({ 'max': 9, 'min': 8 });
 
+
+function formatNumber(value) {
+               return value.replace(/\D/g, "")
+}
 const customerValidators = [
                body('full_name').trim().escape().notEmpty({ ignore_whitespace: true }).isString(),
                body('email').isEmail(),
                body('email_confirmation').isEmail(),
-               body('cpf').isLength({ max: cpfMaxMin.max }).customSanitizer((value) => { return value.replace(/\D/g, "") })
+               body('cpf').isLength({ max: cpfMaxMin.max }).customSanitizer(formatNumber)
                               .matches(`\\d{${cpfMaxMin.min}}$`).custom(async value => {
                                              cpf.isValid(value)
                               }),
-               body('cellphone').isLength({ max: phoneMaxMin.max }).customSanitizer((value) => { return value.replace(/\D/g, "") }).matches(`\\d{${phoneMaxMin.min}}$`),
+               body('cellphone').isLength({ max: phoneMaxMin.max }).customSanitizer(formatNumber).matches(`\\d{${phoneMaxMin.min}}$`),
                body('whatsapp').isBoolean().withMessage('only boolean value'),
                body('email_sms').isBoolean().withMessage('only boolean value'),
 
                body('birthdate').notEmpty().trim().toDate().withMessage('only datetime values'),
                body('country').isAlpha().notEmpty(),
                body('city').isAlpha().notEmpty(),
-               body('postal_code').isLength({ max: postalCodeMaxMin.max }).customSanitizer((value) => { return value.replace(/\D/g, "") }).matches(`\\d{${postalCodeMaxMin.min}}$`).custom(async value => {
+               body('postal_code').isLength({ max: postalCodeMaxMin.max }).customSanitizer(formatNumber).matches(`\\d{${postalCodeMaxMin.min}}$`).custom(async value => {
                               isValidCep(value)
                }),
                body('address').exists({ checkFalsy: true }).notEmpty().isString(),
